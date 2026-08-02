28db0494629d8b5d881f88bf3ec8f9029904a1a7
Rezaghaffarnezhad <0tiamo41@gmail.com>
2026-08-02 18:01:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,26 @@ import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# توکن ربات خود را اینجا وارد کنید
+# توکن ربات شما
 TOKEN = "8186173027:AAHY6oTA7TF0NWbgD_KaajJgqBZtyAu6EPc"
 
-# لینک اینترنتی فایل index.html که روی گیت‌هاب یا هاست آپلود کرده‌اید را اینجا قرار دهید
-WEB_APP_URL = "8186173027:AAHY6oTA7TF0NWbgD_KaajJgqBZtyAu6EPc"
+# لینک صفحه index.html روی گیت‌هاب شما
+WEB_APP_URL = "https://rezaghaffarnezhad.github.io/radar-cyberx/index.html"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # ساخت دکمه شیشه‌ای ورود به اپلیکیشن
     keyboard = [
-        [InlineKeyboardButton("ورود به اپلیکیشن", web_app=WebAppInfo(url=WEB_APP_URL))]
+        [InlineKeyboardButton("ورود به محیط اپ", web_app=WebAppInfo(url=WEB_APP_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
-    # ارسال پیام خوش‌آمدگویی و نمایش دکمه
     await update.message.reply_text(
-        "سلام! برای ورود به نقشه و چت آنلاین روی دکمه زیر کلیک کنید:",
+        "سلام! برای ورود به محیط برنامه روی دکمه زیر کلیک کنید:",
         reply_markup=reply_markup
     )
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
-    
-    # ثبت دستور start
     application.add_handler(CommandHandler("start", start))
-    
-    # اجرای ربات
     application.run_polling()
 
 if __name__ == "__main__":
